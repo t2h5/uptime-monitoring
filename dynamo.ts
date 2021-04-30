083d0f9ab571089e0cda41b71974e963a52d5fd8
tatsuya <t2h5k0@gmail.com>
2021-04-30 23:17:29 +0900

    update eslint packages

--- dynamo.ts
+++ dynamo.ts
@@ -2,12 +2,12 @@ import {
   DynamoDBClient,
   GetItemInput,
   PutItemInput,
-} from '@aws-sdk/client-dynamodb-v2-node'
-import { GetItemCommand } from '@aws-sdk/client-dynamodb-v2-node/commands/GetItemCommand'
-import { PutItemCommand } from '@aws-sdk/client-dynamodb-v2-node/commands/PutItemCommand'
+} from '@aws-sdk/client-dynamodb-v2-node';
+import { GetItemCommand } from '@aws-sdk/client-dynamodb-v2-node/commands/GetItemCommand';
+import { PutItemCommand } from '@aws-sdk/client-dynamodb-v2-node/commands/PutItemCommand';
 
-const dynamoDB = new DynamoDBClient({})
-const dynamoDBTable = process.env.dynamoDBTable
+const dynamoDB = new DynamoDBClient({});
+const dynamoDBTable = process.env.dynamoDBTable;
 
 export async function getState(): Promise<string> {
   const params: GetItemInput = {
@@ -15,20 +15,20 @@ export async function getState(): Promise<string> {
       key: { S: 'state' },
     },
     TableName: dynamoDBTable,
-  }
-  const getItemCommand = new GetItemCommand(params)
+  };
+  const getItemCommand = new GetItemCommand(params);
   return await dynamoDB
     .send(getItemCommand)
     .then((res) => {
       if (res.Item !== undefined) {
-        return res.Item.value.S
+        return res.Item.value.S;
       } else {
-        return 'not found'
+        return 'not found';
       }
     })
     .catch((err) => {
-      throw err
-    })
+      throw err;
+    });
 }
 
 export async function setState(state: string) {
@@ -39,9 +39,9 @@ export async function setState(state: string) {
       value: { S: state },
     },
     TableName: dynamoDBTable,
-  }
-  const putItemCommand = new PutItemCommand(params)
+  };
+  const putItemCommand = new PutItemCommand(params);
   await dynamoDB.send(putItemCommand).catch((err) => {
-    console.log(err)
-  })
+    console.log(err);
+  });
 }
--- handler.ts
+++ handler.ts
@@ -1,19 +1,19 @@
-import { Context, ScheduledEvent, ScheduledHandler } from 'aws-lambda'
-import axios, { AxiosRequestConfig } from 'axios'
-import 'source-map-support/register'
-import { getState, setState } from './dynamo'
+import { Context, ScheduledEvent, ScheduledHandler } from 'aws-lambda';
+import axios, { AxiosRequestConfig } from 'axios';
+import 'source-map-support/register';
+import { getState, setState } from './dynamo';
 
-const targetEndpoint = process.env.targetEndpoint
-const slackWebhookUrl = process.env.slackWebhookUrl
+const targetEndpoint = process.env.targetEndpoint;
+const slackWebhookUrl = process.env.slackWebhookUrl;
 
-let state: string
-let prevState: string
+let state: string;
+let prevState: string;
 
 const colors: { [key: string]: string } = {
   error: 'warning',
   healthy: 'good',
   unhealthy: 'danger',
-}
+};
 
 export const main: ScheduledHandler = async (
   _event: ScheduledEvent,
@@ -21,35 +21,35 @@ export const main: ScheduledHandler = async (
 ) => {
   await getState()
     .then((value) => {
-      prevState = value
+      prevState = value;
     })
     .catch((err) => {
-      console.log(err)
-      prevState = 'unknown'
-    })
+      console.log(err);
+      prevState = 'unknown';
+    });
   const targetConfig: AxiosRequestConfig = {
     headers: { 'User-Agent': 'uptime-monitoring' },
     timeout: 5000,
     validateStatus: (_status) => {
-      return true
+      return true;
     },
-  }
+  };
   await axios
     .get(targetEndpoint, targetConfig)
     .then((res) => {
       if (res.status === 200) {
-        state = 'healthy'
+        state = 'healthy';
       } else {
-        state = 'unhealthy'
+        state = 'unhealthy';
       }
     })
     .catch((err) => {
-      console.log(err)
-      state = 'error'
-    })
-  await setState(state)
+      console.log(err);
+      state = 'error';
+    });
+  await setState(state);
   if (state !== prevState) {
-    console.log(`state changed: ${prevState} to ${state}`)
+    console.log(`state changed: ${prevState} to ${state}`);
     const message: string = JSON.stringify({
       attachments: [
         {
@@ -58,15 +58,15 @@ export const main: ScheduledHandler = async (
           title: 'uptime-monitoring',
         },
       ],
-    })
+    });
     const slackConfig: AxiosRequestConfig = {
       headers: { 'Content-type': 'application/json' },
       timeout: 2000,
-    }
+    };
     await axios.post(slackWebhookUrl, message, slackConfig).catch((err) => {
-      console.log(err)
-    })
+      console.log(err);
+    });
   } else {
-    console.log(`state stayed ${state}`)
+    console.log(`state stayed ${state}`);
   }
-}
+};
